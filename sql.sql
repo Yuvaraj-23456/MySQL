02a9a040b9f1548bbb362498826bb17b433aa15c
Yuvaraj-23456 <113894583+Yuvaraj-23456@users.noreply.github.com>
2024-02-15 11:35:42 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,10 @@
+#use hr employees
+#1.Display the first_name and e_mail if from emplpyees when it sorted in ascending order
+use hr;
+select * from employees;
+select first_name,last_name,email from employees order by first_name;
+#2.List all the top 10 employees sorted by their hire date(earlier should come first. If 2 employees have same  hire_date, The one with the lowest salary should come first.
+select * from employees;
+select 
+#3.List the numbers of employees whose job id first leter is same as the fianl lettrr of their first_name or last_name.alter
+#4.
